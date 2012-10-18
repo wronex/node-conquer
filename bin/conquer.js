8febb82757eff96ed339a7e3a0bfb73187e68436
wronex <github@wronex.com>
2012-10-18 16:19:25 +0200

    comment and logging update

--- bin/conquer.js
+++ bin/conquer.js
@@ -8,17 +8,116 @@ var watchr	= require('watchr'),
 	logger	= require('./logger.js');
 
 var extensions	= ['.js', '.json', '.coffee'], // All file extensions to watch.
-	appPath 	= '', 		// Path to the script.
-	app 		= 'app.js', // The script to run. 
+	appPath 	= './', 	// Path to the script.
+	app 		= 'app.js', // The script to run.
 	appParams 	= [], 		// Parameters that will be sent to the parser.
-	parser 		= 'node' 	// The parser that should run the script.
+	parser 		= 'node'	// The parser that should run the script.
 	instance	= null; 	// Instance of the parser process.
+
+/**
+ * Parses the supplied string of comma seperated file extensions and returns a 
+ *   list.
+ * @param {String} str - a string on the format ".ext1,.ext2,.ext3".
+ * @retruns {String[]} - a list of all the found extensions in @a str.
+ */
+function extensionsParser(str) {
+	// Convert the file extensions string to a list.
+	var list = str.split(',');
+	for (var i = 0; i < list.length; i++) {
+		// Make sure the file extension has the correct format: '.ext'
+		var ext = '.' + list[i].replace(/(^\s?\.?)|(\s?$)/g, '');
+		list[i] = ext.toLowerCase();
+	}
+	console.log('%j', list)
+	return list;
+}
+
+/** 
+ * Kills the parser. 
+ * @param {Boolean} [noMsg] - indicates if no message should be written to the 
+ *   log. Defaults to false.
+ */
+function kill(noMsg) {
+	if (!instance)
+		return;
+
+	if ((noMsg || false) !== true)
+		logger.log('Killed', clc.green(app));
+	
+	instance.kill();
+	instance = null;
+}
+
+/** Restarts the parser. */
+function restart() {
+	logger.log('Restarting', clc.green(app));
+	kill(true);
+	start(true);
+}
+
+/** 
+ * Starts and instance of the parser if none is running.
+ * @param {Boolean} [noMsg] - indicates if no message should be written to the 
+ *   log. Defaults to false.
+ */
+function start(noMsg) {
+	if ((noMsg || false) !== true)
+		logger.log('Starting', clc.green(app), 'with', clc.magenta(parser));
+	
+	if (instance)
+		return;
+	
+	// Spawn an instance of the parser that will run the app.
+	instance = spawn(parser, [app].concat(appParams));
+	
+	// Redirect the parser/app's output to the console.
+	instance.stdout.on('data', function (data) { logger.appLog(app, data.toString()); });
+	instance.stderr.on('data', function (data) { logger.appLog(app, data.toString(), true); });
+	instance.stderr.on('data', function (data) {
+		if (/^execvp\(\)/.test(data.toString())) {
+			logger.error('Failed to restart child process.');
+			process.exit(0);
+		}
+	});
+	instance.on('exit', function (code, signal) {
+		if (signal == 'SIGUSR2') {
+			logger.error('Signal interuption.');
+			restart();
+		};
+	});
+}
+
+// Listen for uncaught exceptions that might be thrown by this script.
+process.on('uncaughtException', function(error){
+	logger.error(error.stack.toString());
+	restart();
+});
+
+// Make sure to kill the parser process when this process is killed.
+process.on('exit', function(code) {
+	kill();
+});
+
+if (process.platform.substr(0, 3) !== 'win') {
+	process.on('SIGINT', function() {
+		logger.warn('User killed process');
+		kill();
+		process.exit(0);
+	});
+
+	process.on('SIGTERM', function() {
+		kill();
+		process.exit(0);
+	});
+}
 
 // Configure commander for the commands it should accept from the user.
 program
-	.version('1.0.1')
+	.version('1.0.2')
 	.usage('[options] <app.js> [app options]')
-	.option('-e, --extensions [".js,.txt"]', 'watch for changes to these file extensions');
+	.option('-e, --extensions [".js,.txt"]', 
+			'watch for changes to these file extensions', 
+			extensionsParser);
 
 program.on('--help', function(){
 	console.log('  Example:');
@@ -41,6 +140,9 @@ if (program.args.length == 0) {
 }
 app = program.args[0];
 appPath = path.dirname(app);
+
+// All arguments after the input file (app) are app parameters that will be 
+// passed to the parser.
 appParams = process.argv.slice(process.argv.indexOf(app) + 1);
 
 // Select parser based on file extension.
@@ -51,68 +153,16 @@ if (path.extname(app) == '.coffee') {
 }
 
 if (program.extensions) {
-	// Convert the file extensions to a list.
-	extensions = program.extensions.split(',');
-	for (var i = 0; i < program.extensions.length; i++) {
-		// Make sure the file extension has the correct format: '.ext'
-		var ext = '.' + program.extensions[i].replace(/(^\s?\.?)|(\s?$)/g, '');
-		program.extensions[i] = ext.toLowerCase();
-	}
-	
-	logger.log('Watching extensions: ' + clc.green(program.extensions.join(', ')));
-}
-
-/** Kills the parser. */
-function kill(noMsg) {
-	if (!instance)
-		return;
-
-	if ((noMsg || false) !== true)
-		logger.log('Killed', clc.green(app));
-	
-	instance.kill();
-	instance = null;
-}
-
-/** Restarts the parser. */
-function restart() {
-	logger.log('Restarting', clc.green(app));
-	kill(true);
-	start(true);
-}
-
-/** Starts and instance of the parser. */
-function start(noMsg) {
-	if ((noMsg || false) !== true)
-		logger.log('Starting', clc.green(app), 'with', clc.magenta(parser));
-	
-	if (instance)
-		return;
-	
-	// Spawn an instance of the parser.
-	instance = spawn(parser, [app].concat(appParams));
-	
-	// Redirect the parser's output to the console.
-	instance.stdout.on('data', function (data) { logger.appLog(app, data.toString()); });
-	instance.stderr.on('data', function (data) { logger.appError(app, data.toString()); });
-	instance.stderr.on('data', function (data) {
-		if (/^execvp\(\)/.test(data.toString())) {
-			logger.error('Failed to restart child process.');
-			process.exit(0);
-		}
-	});
-	instance.on('exit', function (code, signal) {
-		if (signal == 'SIGUSR2') {
-			logger.error('Signal interuption.');
-			restart();
-		};
-	});
+	extensions = program.extensions;
+	logger.log('Watching extensions: ' + clc.green(extensions.join(', ')));
 }
 
 // Watch the directory supplied by the user.
 watchr.watch({
 	path: appPath,
 	listener: function(eventName, filePath, fileCurrentStat, filePreviousStat) {
+		// We are only interesed in files with extensions that are part of the 
+		// extensions array.
 		var ext = path.extname(filePath).toLowerCase();
 		if (extensions.indexOf(ext) != -1) {
 			logger.log(clc.green(path.basename(filePath)), 'changed');
@@ -127,28 +177,3 @@ watchr.watch({
 		start();
 	}
 });
-
-// Listen for uncaught exceptions that might be thrown by this script.
-process.on('uncaughtException', function(error){
-	logger.error(error.toString());
-	logger.error(error.stack.toString());
-	restart();
-});
-
-// Make sure to kill the parser process when this process is killed.
-process.on('exit', function(code) {
-	kill();
-});
-
-if (process.platform.substr(0, 3) !== 'win') {
-	process.on('SIGINT', function() {
-		logger.warn('User killed process');
-		kill();
-		process.exit(0);
-	});
-
-	process.on('SIGTERM', function() {
-		kill();
-		process.exit(0);
-	});
-}
--- bin/logger.js
+++ bin/logger.js
@@ -3,36 +3,57 @@
 var clc		= require('cli-color'),
 	util	= require('util');
 
+/**
+ * Writes the supplied message to the log.
+ * @param {String} msg - the message.
+ * @param {Function} [color] - the clc coloring function to use on each message.
+ * @param {String} [prefix] - a message prefix. Defaults to '[conquer]'.
+ */
+function write(msg, color, prefix) {
+	if (!prefix)
+		prefix = clc.cyan('[conquer]');
+
+	// Print each line of the message on its own line.
+	var messages = msg.split('\n');
+	for (var i = 0; i < messages.length; i++) {
+		var message = messages[i].replace(/(\s?$)|(\n?$)/gm, '');
+		if (message && message.length > 0)
+			console.log(prefix, color ? color(message) : message);
+	}
+}
+
 /** Writes the supplied parameters to the log. */
 function log() {
-	arguments[0] = clc.cyan('[conquer]') + ' ' + arguments[0];
-	console.log.apply(console, arguments);
+	var msg = util.format.apply(null, Array.prototype.slice.call(arguments, 0));
+	write(msg);
 }
 
 /** Writes the supplied parameters to the log as a warning. */
 function warn() {
-	log(clc.yellow(util.format.apply(null, Array.prototype.slice.call(arguments, 0))));
+	var msg = util.format.apply(null, Array.prototype.slice.call(arguments, 0));
+	write(msg, clc.yellow);
 }
 
 /** Writes the supplied parameters to the log as an error. */
 function error() {
-	log(clc.red(util.format.apply(null, Array.prototype.slice.call(arguments, 0))));
+	var msg = util.format.apply(null, Array.prototype.slice.call(arguments, 0));
+	write(msg, clc.red);
 }
 
-function appLog(app, msg) {
-	app = clc.yellow('[' + app + ']');
-	msg = msg.replace(/\n?$/g, '');
-	console.log.apply(console, arguments, ' ', msg);
+/**
+ * Writes the supplied message from the running application to the log.
+ * @param {String} app - the name of the running application.
+ * @param {String} msg - the message.
+ * @param {Boolean} isError - indicates if the message is an error message.
+ */
+function appLog(app, msg, isError) {
+	write(msg, 
+		isError ? clc.red : null, 
+		clc.yellow('[' + app + ']'));
 }
 
-function appError(app, msg) {
-	appLog(app, clc.red(msg));
-}
-
-
-// Public methods:
+// Public interface:
 exports.log = log;
 exports.warn = warn;
 exports.error = error;
 exports.appLog = appLog;
-exports.appError = appError;
